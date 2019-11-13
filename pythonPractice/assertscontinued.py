6bf82fd4383b0b1b53d709f482e8d7fcd9214159
mattyturn95 <mattyturn95@gmail.com>
2019-11-13 18:39:13 +0000

    Test driven deployment / Assertions

--- pythonPractice/assertscontinued.py
+++ pythonPractice/assertscontinued.py
@@ -1,31 +1,22 @@
-def even_number_of_evens(numbers): 
-
-    # Check to see if the list is empty
-    if numbers == [2, 4, 6,]:
-        return True
+def even_number_evens(numbers):
+    if numbers == []:
+       return False
     else:
-        # Set a `number_of_evens` variable that will be incremented each time
-        # an even number is found
         evens = 0
-        
-    # Iterate of over each item and if it's an even number, increment the
-    # `evens` variable
-    for number in numbers:
-        if number % 2 == 0:
+
+    for n in numbers:
+        if n % 2 == 0:
             evens += 1
     
-    if evens == 3:
+    if evens == 0:
         return False
-    else:
-        return evens % 2 == 0
+    
+    else:            
+        return evens %2 == 0
 
-#assert even_number_of_evens([]) == False, "No numbers"
-assert even_number_of_evens([2, 4]) == True, "Two Even Numbers"
-#assert even_number_of_evens([2]) == False, "One even number"
-#assert even_number_of_evens([1, 2, 3, 4]) == True, "Four Numbers, Two are even"
-#assert even_number_of_evens([1, 3, 5, 7, 9, 11, 13, 15, 17, 19]) == False, "Even amount of numbers - none are even numbers though"
-#assert even_number_of_evens([7, 9 , 3, 5]) == False, "No even Numbers"
-#assert even_number_of_evens([!%%$$@@!!@$]) == False, "Special Characters"
-#assert even_number_of_evens([22, 44, 88, 176]) == True, "four even numbers"
+assert even_number_evens([]) == False, "No Numbers"
+assert even_number_evens([2, 4]) == True, "Two evens"
+assert even_number_evens([2]) == False, "One even Number"
+assert even_number_evens([1, 3, 9]) == False, "Uneven Number of Odd Numbers"
 
-print("all tests have passed")
+print("All tests Passed")
